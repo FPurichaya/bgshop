27ac5d01fa26664c7f9a9f6eb7ac765b7dc24b8c
Jeep <jeep@Mac.fritz.box>
2020-06-09 10:45:18 +0200

    added href

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 const Featured = ({ featured }) => (
   <span>
     {featured ? (
-      <a className="ui right yellow corner label">
+      <a href="http://duckduckgo.com" className="ui right yellow corner label">
         <i className="star icon"></i>
       </a>
     ) : (
-      <a className="ui right corner label">
+      <a href="http://duckduckgo.com" className="ui right corner label">
         <i className="star icon"></i>
       </a>
     )}
