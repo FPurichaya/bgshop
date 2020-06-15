b3685cbd27926dfc2f12df8b36d6268a16ced328
Jeep <sopeach.a@gmail.com>
2020-06-15 22:24:37 +0200

    managed type

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -4,6 +4,9 @@ class GameForm extends Component {
   state = {
     name: '',
     description: '',
+    price: 0,
+    duration: 0,
+    players: '',
   };
 
   //define handleSubmit  as a class property
@@ -14,7 +17,10 @@ class GameForm extends Component {
 
   //Universal event handler
   //To make it universal, we need to identify from which component data came from. So, we can use name method before it.
-  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+  handleStringChange = (e) =>
+    this.setState({ [e.target.name]: e.target.value });
+  handleNumberChange = (e) =>
+    this.setState({ [e.target.name]: parseInt(e.target.value, 10) });
 
   render() {
     return (
@@ -28,12 +34,12 @@ class GameForm extends Component {
             name="name"
             placeholder="Full game title"
             value={this.state.name}
-            onChange={this.handleChange}
+            onChange={this.handleStringChange}
           />
         </div>
 
         <div className="field">
-          <label htmlFor="description ">Game Description</label>
+          <label htmlFor="description">Game Description</label>
           <textarea
             type="text"
             id="description "
@@ -41,8 +47,46 @@ class GameForm extends Component {
             name="description"
             placeholder="Game Description"
             value={this.state.description}
-            onChange={this.handleChange}
+            onChange={this.handleStringChange}
           />
+        </div>
+
+        <div className="three fields">
+          <div className="field">
+            <label htmlFor="price">Price</label>
+            <input
+              type="number"
+              id="price"
+              //add name to make universal handler
+              name="price"
+              value={this.state.price}
+              onChange={this.handleNumberChange}
+            />
+          </div>
+
+          <div className="field">
+            <label htmlFor="duration">duration (in minutes)</label>
+            <input
+              type="number"
+              id="duration"
+              //add name to make universal handler
+              name="duration"
+              value={this.state.duration}
+              onChange={this.handleNumberChange}
+            />
+          </div>
+
+          <div className="field">
+            <label htmlFor="players">Players</label>
+            <input
+              type="text"
+              id="players"
+              //add name to make universal handler
+              name="players"
+              value={this.state.players}
+              onChange={this.handleStringChange}
+            />
+          </div>
         </div>
 
         <button className="ui button" type="submit">
