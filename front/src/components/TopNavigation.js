340fb0bdf4b6dbb70942c0a2c48fe2d692cf30aa
Jeep <sopeach.a@gmail.com>
2020-06-25 16:17:01 +0200

    navigate between route with link component

--- front/src/components/TopNavigation.js
+++ front/src/components/TopNavigation.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 
-const TopNavigation = ({ showGameForm }) => (
+const TopNavigation = () => (
   <div className="ui secondary pointing menu">
-    <a href="/" className="item">
+    <NavLink exact to="/" className="item">
       BGShop
-    </a>
-    {/* eslint-disable-next-line */}
-    <a className="item" onClick={showGameForm}>
+    </NavLink>
+    <NavLink exact to="/games" className="item">
+      Games
+    </NavLink>
+    <NavLink exact to="/games/new" className="item">
       <i className="icon plus"></i>Add New Game{' '}
-    </a>
+    </NavLink>
   </div>
 );
-
-TopNavigation.propTypes = {
-  showGameForm: PropTypes.func.isRequired,
-};
 
 export default TopNavigation;
