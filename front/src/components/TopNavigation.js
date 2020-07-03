a57f73970e4e3717486f5e3234f51fcb647603f4
Jeep <sopeach.a@gmail.com>
2020-07-03 15:58:50 +0200

    dummy authentication

--- front/src/components/TopNavigation.js
+++ front/src/components/TopNavigation.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 
-const TopNavigation = () => (
+const TopNavigation = ({ isAuthenticated, logout }) => (
   <div className="ui secondary pointing menu">
     <NavLink exact to="/" className="item">
       BGShop
@@ -12,7 +13,29 @@ const TopNavigation = () => (
     <NavLink exact to="/games/new" className="item">
       <i className="icon plus"></i>Add New Game{' '}
     </NavLink>
+
+    {isAuthenticated ? (
+      <div className="right menu">
+        <a onClick={logout} className="item">
+          Log out
+        </a>
+      </div>
+    ) : (
+      <div className="right menu">
+        <NavLink to="/signup" className="item">
+          Signup
+        </NavLink>
+        <NavLink to="/login" className="item">
+          Login
+        </NavLink>
+      </div>
+    )}
   </div>
 );
+
+TopNavigation.propTypes = {
+  isAuthenticated: PropTypes.string.isRequired,
+  logout: PropTypes.func.isRequired,
+};
 
 export default TopNavigation;
