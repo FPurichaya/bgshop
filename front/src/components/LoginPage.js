b167209e466c8e4c85d2ee05557bec0f0f8e4878
Jeep <sopeach.a@gmail.com>
2020-07-06 21:11:18 +0200

    added login form

---
+++ front/src/components/LoginPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import LoginForm from './LoginForm';
+import api from '../api';
+
+class LoginPage extends Component {
+  submit = (data) => api.users.login(data).then((token) => console.log(token));
+
+  render() {
+    return (
+      <div className="ui segment">
+        <LoginForm submit={this.submit} />
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
