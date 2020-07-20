61e00f6d03b3360d9b554cb9063325b2bd68e00c
Jeep <sopeach.a@gmail.com>
2020-07-20 22:02:08 +0200

    Repair Authorization for youtube

--- front/src/api.js
+++ front/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = `https://api.purichaya.fraczyk.eu`;
+// axios.defaults.baseURL = `https://api.purichaya.fraczyk.eu`;
 
 export default {
   store: {
